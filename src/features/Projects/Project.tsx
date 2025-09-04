33187353ed403a94055604313597e92aea9f6b09
SuperCoolNinja <sbpro570@gmail.com>
2025-09-04 21:23:32 +0200

    change root to index.css

--- src/features/Projects/Project.tsx
+++ src/features/Projects/Project.tsx
@@ -3,12 +3,12 @@ import styles from "./project.module.css";
 const Project: React.FunctionComponent = () => {
   return (
     <div className={styles.container}>
-      <div className={styles.wrapper_title}>
+      <section className={styles.wrapper_title}>
         <h1 className={styles.title}>Projects</h1>
         <p className={styles.subtitle}>
           A list of projects that I’ve worked on.
         </p>
-      </div>
+      </section>
     </div>
   );
 };
