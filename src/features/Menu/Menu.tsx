950b6a50c3741f6675711645ca0c3137f3ed68a0
SuperCoolNinja <sbpro570@gmail.com>
2025-09-10 13:35:17 +0200

    stylised the new menu bar system with glass effect ui

--- src/features/Menu/Menu.tsx
+++ src/features/Menu/Menu.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import type { MenuProps } from "../../interfaces/IMenuProps";
 import styles from "./menu.module.css";
 
@@ -6,8 +7,39 @@ const Menu: React.FunctionComponent<MenuProps> = ({
   selected,
   onSelect,
 }) => {
+  const listRef = useRef<HTMLUListElement>(null);
+  const highlightRef = useRef<HTMLSpanElement>(null);
+
+  const updateHighlight = () => {
+    const el = document.querySelector<HTMLLIElement>(
+      `.${styles.item}.${styles.selected}`
+    );
+    if (el && listRef.current && highlightRef.current) {
+      const rect = el.getBoundingClientRect();
+      const parentRect = listRef.current.getBoundingClientRect();
+
+      const extraWidth = 100 / 2;
+      const totalWidth = rect.width + extraWidth;
+      const offset = extraWidth / 2;
+
+      highlightRef.current.style.width = `${totalWidth}px`;
+      highlightRef.current.style.left = `${
+        rect.left - parentRect.left - offset
+      }px`;
+    }
+  };
+
+  useEffect(() => {
+    updateHighlight();
+
+    window.addEventListener("resize", updateHighlight);
+    return () => {
+      window.removeEventListener("resize", updateHighlight);
+    };
+  }, [selected]);
+
   return (
-    <ul className={styles.list}>
+    <ul className={styles.list} ref={listRef}>
       {items.map((v) => {
         const isSelected = v === selected;
         return (
@@ -20,7 +52,9 @@ const Menu: React.FunctionComponent<MenuProps> = ({
           </li>
         );
       })}
+      <span className={styles.highlight} ref={highlightRef}></span>
     </ul>
   );
 };
+
 export default Menu;
