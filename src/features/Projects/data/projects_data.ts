64f42fe94285c5aa1d4930e5b54806403d7d92ef
SuperCoolNinja <sbpro570@gmail.com>
2025-09-13 16:43:05 +0200

    need to integrate a carousel

--- src/features/Projects/data/projects_data.ts
+++ src/features/Projects/data/projects_data.ts
@@ -38,6 +38,36 @@ export const projects: IProjectsData[] = [
 
   {
     id: generateUniqueId(),
+    name: "Authentication",
+    description:
+      "An authentication system I created that allowed me to deepen my understanding of API, backend, and security through the use of JWT.",
+    tag: "Fullstack",
+    year: "2023",
+    stacks: ["Html", "Sass", "Typescript", "NodeJS", "JWT", "ReactJS"],
+    image:
+      "https://image.noelshack.com/fichiers/2024/26/3/1719430935-screenshot-2024-06-26-214215.png",
+    category: "Web",
+    source_code: "https://github.com/SuperCoolNinja/auth",
+    ...tagColors["Fullstack"],
+  },
+  {
+    id: generateUniqueId(),
+    name: "Paint Web App",
+    description:
+      "A board where you can draw using a provided color palette, change your brush size, and erase everything, all with a modern and compact design.",
+    tag: "Frontend",
+    year: "2021",
+    stacks: ["Html", "Css", "Javascript", "Canvas"],
+    image:
+      "https://image.noelshack.com/fichiers/2024/26/3/1719431038-screenshot-2024-06-26-214350.png",
+    category: "Web",
+    source_code: "https://github.com/SuperCoolNinja/Paint-Web-App",
+    demo: "https://supercoolninja.github.io/Paint-Web-App/",
+    ...tagColors["Frontend"],
+  },
+
+  {
+    id: generateUniqueId(),
     name: "Pong Game",
     description: "a Pong Game made to learn and practise basic 2D Math Logic.",
     tag: "Game Dev",
@@ -47,6 +77,21 @@ export const projects: IProjectsData[] = [
       "https://image.noelshack.com/fichiers/2024/26/3/1719431132-screenshot-2024-06-26-214537.png",
     category: "game development",
     source_code: "github.com/SuperCoolNinja/love2D-pongGame",
+    ...tagColors["Gamedev"],
+  },
+
+  {
+    id: generateUniqueId(),
+    name: "FLAPPYFLYTIME",
+    description:
+      "A Flappy Bird clone created to learn the basics of the Unity API.",
+    tag: "Game Dev",
+    year: "2022",
+    stacks: ["C#", "Unity3D"],
+    image:
+      "https://image.noelshack.com/fichiers/2024/26/3/1719431180-screenshot-2024-06-26-214628.png",
+    category: "game development",
+    demo: "https://supercoolninja.itch.io/flappyflytime",
     ...tagColors["Gamedev"],
   },
 ];
