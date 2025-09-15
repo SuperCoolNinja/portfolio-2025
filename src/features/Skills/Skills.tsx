6e902eedc75470b808e7a487553925ea2e1a9406
SuperCoolNinja <sbpro570@gmail.com>
2025-09-15 14:06:51 +0200

    skills iteration 1 finish

--- src/features/Skills/Skills.tsx
+++ src/features/Skills/Skills.tsx
@@ -1,7 +1,59 @@
+import type { JSX } from "react";
+import Card from "../../components/Card/Card";
+import Tag from "../../components/Tag/Tag";
+import { Colors } from "../../utils/colors";
+import styles from "./skills.module.css";
+import { Cookie, Gamepad2, Shapes, SquareChevronRight } from "lucide-react";
+
 const Skills: React.FunctionComponent = () => {
+  const FRONT = ["JAVASCRIPT", "REACTJS", "CSS", "TAILWINDCSS", "FIGMA"];
+  const BACK = ["TYPESCRIPT", "MONGODB", "EXPRESSJS", "NODEJS"];
+  const MODS = ["LUA", "C++", "FIVEM", "GTA V"];
+  const GAMEDEV = ["C#", "C", "LUA", "UNITY3D", "LOVE2D", "RAYLIB"];
+  const TOOLS = ["GIT", "AGILE/SCRUM", "VSCODE", "VISUAL STUDIO", "POSTMAN"];
+
+  const renderCard = (icon: JSX.Element, title: string, tags: string[]) => (
+    <div className={styles.card_container}>
+      <Card style={{ marginTop: "5rem", padding: "1rem" }}>
+        <section className={styles.card_wrapper_title}>
+          {icon}
+          <h3>{title}</h3>
+        </section>
+        <section className={styles.card_wrapper_tags}>
+          {tags.map((v) => (
+            <Tag
+              key={v}
+              style={{
+                fontFamily: "system-ui",
+                fontWeight: "600",
+                padding: "0.5rem 1rem",
+                backgroundColor: Colors[v]?.backgroundColor ?? "",
+                color: Colors[v]?.color ?? "",
+              }}
+              label={v}
+            />
+          ))}
+        </section>
+      </Card>
+    </div>
+  );
+
   return (
-    <div>
-      <h3>Skills</h3>
+    <div className={styles.container}>
+      <section className={styles.wrapper_title_subtitle}>
+        <h3 className={styles.title}>Skills</h3>
+        <p className={styles.subtitle}>
+          From web projects to game creation and modding.
+        </p>
+      </section>
+
+      <div className={styles.card_wrapper}>
+        {renderCard(<Cookie />, "Frontend", FRONT)}
+        {renderCard(<SquareChevronRight />, "Backend", BACK)}
+        {renderCard(<Gamepad2 />, "Game dev", GAMEDEV)}
+        {renderCard(<Shapes />, "Modding", MODS)}
+        {renderCard(<Gamepad2 />, "Tools & Technologies", TOOLS)}
+      </div>
     </div>
   );
 };
