fe86903c9e80919dcf0c1c447657f783c5f680d4
SuperCoolNinja <sbpro570@gmail.com>
2025-09-16 23:37:16 +0200

    font size level up

--- src/features/About/data/aboutItem_data.ts
+++ src/features/About/data/aboutItem_data.ts
@@ -13,6 +13,5 @@ export const aboutItemsData: IAboutItems[] = [
 
 export const AboutMeSentances: string[] = [
   "Self-taught fullstack developer from France who enjoy turning ideas into small projects to learn.",
-  "Currently working with Typescript, ReactJS, NodeJS, and MongoDB, while always seeking to improve.",
   "Also interested in game development and clean, user-friendly UX/UI design.",
 ];
