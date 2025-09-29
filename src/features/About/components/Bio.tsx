25d2d3a2d735a7878e02d694ad1dffa203548560
SuperCoolNinja <sbpro570@gmail.com>
2025-09-29 20:54:56 +0200

    frontend focus but also fullstack

--- src/features/About/components/Bio.tsx
+++ src/features/About/components/Bio.tsx
@@ -4,7 +4,7 @@ const Bio: React.FunctionComponent = () => {
   return (
     <>
       <h2 className={styles.name}>Samir Baatour</h2>
-      <p className={styles.job}>Fullstack Developer</p>
+      <p className={styles.job}>Frontend Developer</p>
     </>
   );
 };
